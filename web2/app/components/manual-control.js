cc1979544493b6dc0e9692292391763322cf9354
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-02-14 18:51:25 +0300

    Fix JSHint error in ember interface

--- web2/app/components/manual-control.js
+++ web2/app/components/manual-control.js
@@ -2,22 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
    currentHost: function(port) {
-    return location.protocol + '//' + location.hostname + ':' + port
+    return location.protocol + '//' + location.hostname + ':' + port;
   },
 
   didInsertElement: function() {
-      $.get(this.currentHost('2500') + '/devices/list/manual', function( data ) {
-        $.each($.parseJSON(data)["keys"], function(i, key) {
-          $('#selectList').append("<option selected>" + key + "</option>")
+      Ember.$.get(this.currentHost('2500') + '/devices/list/manual', function( data ) {
+        Ember.$.each(Ember.$.parseJSON(data)["keys"], function(i, key) {
+          Ember.$('#selectList').append("<option selected>" + key + "</option>");
         });
       });
   },
 
   actions: {
     select: function() {
-      var deviceId = $('#selectList').find(":selected").text();
-      $('.controlBlock').html('<iframe src="' + this.currentHost('3900') + '/'
-        + deviceId + '"  width="400" height="500"></iframe>')
+      var deviceId = Ember.$('#selectList').find(":selected").text();
+      Ember.$('.controlBlock').html('<iframe src="' + this.currentHost('3900') + '/'+ deviceId + '"  width="400" height="500"></iframe>');
     }
   },
 });
