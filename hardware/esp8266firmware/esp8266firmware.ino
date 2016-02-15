6ffde329cb61537aff057e251ffb3acd90b847b2
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-02-15 23:06:49 +0300

    auto format eps8266firmwarecode

--- hardware/esp8266firmware/esp8266firmware.ino
+++ hardware/esp8266firmware/esp8266firmware.ino
@@ -27,12 +27,12 @@ char buff[255] = "";
 boolean readString(char b) {
   int len = strlen(buff);
 
-  if(len >= 255) {
+  if (len >= 255) {
     return true;
   }
-  
-  if(b == 0x0A) {
-    if(len > 0 && buff[len - 1] == 0x0D) {
+
+  if (b == 0x0A) {
+    if (len > 0 && buff[len - 1] == 0x0D) {
       buff[len - 1] = 0;
       return true;
     }
@@ -44,135 +44,135 @@ boolean readString(char b) {
 }
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t lenght) {
-    switch(type) {
-        case WStype_DISCONNECTED:
-            connected = false;
-            break;
-        case WStype_CONNECTED:
-            connected = true;
-            break;
-        case WStype_TEXT:
-            Serial.printf("%s\r\n", payload);
-            break;
-        case WStype_BIN:
-//            USE_SERIAL.printf("[WSc] get binary lenght: %u\n", lenght);
-            break;
-    }
+  switch (type) {
+    case WStype_DISCONNECTED:
+      connected = false;
+      break;
+    case WStype_CONNECTED:
+      connected = true;
+      break;
+    case WStype_TEXT:
+      Serial.printf("%s\r\n", payload);
+      break;
+    case WStype_BIN:
+      //            USE_SERIAL.printf("[WSc] get binary lenght: %u\n", lenght);
+      break;
+  }
 
 }
 
 int parseString(String string, int index1) {
-  for(i = index1; i<= string.length(); i++) {
-    if(string[i] == '+')
+  for (i = index1; i <= string.length(); i++) {
+    if (string[i] == '+')
       return i;
   }
 }
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println();
+  Serial.begin(9600);
+  Serial.println();
 }
 
 void loop() {
-    if(Serial.available() > 0 && readString(Serial.read())) {
-         string = String(buff);
-         buff[0] = 0;
+  if (Serial.available() > 0 && readString(Serial.read())) {
+    string = String(buff);
+    buff[0] = 0;
 
-         if(string == "AT") {
-           Serial.println("OK");
-           return;
-         }
-              
-         if(string == "AT:status") {
-            if(setuped) {
-              Serial.print("connected to ssid:");
-              Serial.println(ssid);
-            } else {
-              Serial.println("disconnected");
-            }
-            if(connected) {
-              Serial.print("connection with host: ");
-              Serial.print(host);
-              Serial.print(":");
-              Serial.print(String(port));
-              Serial.println(url);
-            }
-            return;
-         }
-
-         if(string == "AT:reset") {
-            if(connected) {
-              webSocket.disconnect();
-            }
-            if(setuped) {
-              WiFi.disconnect();
-              setuped = false;
-            }
-            Serial.println("OK");
-
-            return;
-         }
-
-         if(string.startsWith("AT:setup") && !setuped) {
-              indexA = parseString(string, 0);
-              indexB = parseString(string, indexA + 1);
-              string.substring(indexA + 1, indexB).toCharArray(ssid, indexB - indexA);
-              string.substring(indexB + 1).toCharArray(password, string.length() - indexB);
-
-              WiFi.begin(ssid, password);
-              while (WiFi.status() != WL_CONNECTED && timeout < 100) {
-                delay(100);
-                timeout += 1;
-              }
-              if(WiFi.status() == WL_CONNECTED) {
-                Serial.println("OK");
-                setuped = true;
-              } else {
-                Serial.print("FAIL: WiFi up, ");
-                if(WiFi.status() != WL_CONNECTED)
-                  Serial.print("abort");
-                if(timeout > 100)
-                  Serial.print("timeout");
-                Serial.println();
-              }
-
-              return;
-         }
-
-         if(string.startsWith("AT:connect") && !connected && setuped) {
-              indexA = parseString(string, 0);
-              indexB = parseString(string, indexA + 1);
-              string.substring(indexA + 1, indexB).toCharArray(host, indexB - indexA);
-              indexA = parseString(string, indexB + 1);
-              port = string.substring(indexB + 1, indexA).toInt();
-              string.substring(indexA + 1).toCharArray(url, string.length() - indexA);
-              
-              webSocket.begin(host, port, url);
-              webSocket.onEvent(webSocketEvent);
-
-              timeout = 0;
-              while(!connected) {
-                webSocket.loop();
-                delay(10);
-                timeout += 1;
-                if(timeout > 500) {
-                  Serial.println("FAIL: can't be connected, timeout");
-                  return;
-                }
-              }
-              Serial.println("OK");
-
-              return;
-         }
-
-         if(setuped && connected) {
-            webSocket.sendTXT(string);
-         } else {
-            Serial.println("FAIL: disconnected");
-         }
+    if (string == "AT") {
+      Serial.println("OK");
+      return;
     }
-    if(setuped && connected) {
+
+    if (string == "AT:status") {
+      if (setuped) {
+        Serial.print("connected to ssid:");
+        Serial.println(ssid);
+      } else {
+        Serial.println("disconnected");
+      }
+      if (connected) {
+        Serial.print("connection with host: ");
+        Serial.print(host);
+        Serial.print(":");
+        Serial.print(String(port));
+        Serial.println(url);
+      }
+      return;
+    }
+
+    if (string == "AT:reset") {
+      if (connected) {
+        webSocket.disconnect();
+      }
+      if (setuped) {
+        WiFi.disconnect();
+        setuped = false;
+      }
+      Serial.println("OK");
+
+      return;
+    }
+
+    if (string.startsWith("AT:setup") && !setuped) {
+      indexA = parseString(string, 0);
+      indexB = parseString(string, indexA + 1);
+      string.substring(indexA + 1, indexB).toCharArray(ssid, indexB - indexA);
+      string.substring(indexB + 1).toCharArray(password, string.length() - indexB);
+
+      WiFi.begin(ssid, password);
+      while (WiFi.status() != WL_CONNECTED && timeout < 100) {
+        delay(100);
+        timeout += 1;
+      }
+      if (WiFi.status() == WL_CONNECTED) {
+        Serial.println("OK");
+        setuped = true;
+      } else {
+        Serial.print("FAIL: WiFi up, ");
+        if (WiFi.status() != WL_CONNECTED)
+          Serial.print("abort");
+        if (timeout > 100)
+          Serial.print("timeout");
+        Serial.println();
+      }
+
+      return;
+    }
+
+    if (string.startsWith("AT:connect") && !connected && setuped) {
+      indexA = parseString(string, 0);
+      indexB = parseString(string, indexA + 1);
+      string.substring(indexA + 1, indexB).toCharArray(host, indexB - indexA);
+      indexA = parseString(string, indexB + 1);
+      port = string.substring(indexB + 1, indexA).toInt();
+      string.substring(indexA + 1).toCharArray(url, string.length() - indexA);
+
+      webSocket.begin(host, port, url);
+      webSocket.onEvent(webSocketEvent);
+
+      timeout = 0;
+      while (!connected) {
         webSocket.loop();
+        delay(10);
+        timeout += 1;
+        if (timeout > 500) {
+          Serial.println("FAIL: can't be connected, timeout");
+          return;
+        }
+      }
+      Serial.println("OK");
+
+      return;
     }
+
+    if (setuped && connected) {
+      webSocket.sendTXT(string);
+    } else {
+      Serial.println("FAIL: disconnected");
+    }
+  }
+  if (setuped && connected) {
+    webSocket.loop();
+  }
 }
 
