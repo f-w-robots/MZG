c83191e42dc2ffe4de7ee7591054b69f5e1ad605
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-02-17 23:29:41 +0300

    WEB: Added delete image button

--- web2/app/components/image-input.js
+++ web2/app/components/image-input.js
@@ -1,8 +1,14 @@
 export default Ember.Component.extend({
   file: null,
+
   actions: {
     fileSelectionChanged: function(file) {
-      this.set('file', file)
+      this.set('file', file);
+    },
+
+    removeImage: function(file) {
+      this.$('input[type="file"]').val('');
+      this.set('file', file);
     },
   },
 });
