78e8b7685ccf11419ace2a6e35e1c8a64a7696f2
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-02-18 00:01:54 +0300

    WEB: replace select list of devices to icons

--- web2/app/components/manual-control.js
+++ web2/app/components/manual-control.js
@@ -1,22 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  devices: null,
+  url: null,
+
    currentHost: function(port) {
     return location.protocol + '//' + location.hostname + ':' + port;
   },
 
   didInsertElement: function() {
-      Ember.$.get(this.currentHost('2500') + '/devices/list/manual', function( data ) {
-        Ember.$.each(Ember.$.parseJSON(data)["keys"], function(i, key) {
-          Ember.$('#selectList').append("<option selected>" + key + "</option>");
-        });
-      });
+    var self = this;
+    Ember.$.get(this.currentHost('2500') + '/devices/list/manual', function(data) {
+      self.set('devices', Ember.$.parseJSON(data)["keys"]);
+    });
   },
 
   actions: {
-    select: function() {
-      var deviceId = Ember.$('#selectList').find(":selected").text();
-      Ember.$('.controlBlock').html('<iframe src="' + this.currentHost('3900') + '/'+ deviceId + '"  width="400" height="500"></iframe>');
-    }
+    select: function(deviceId) {
+      this.set('url', this.currentHost('3900') + '/'+ deviceId);
+    },
   },
 });
