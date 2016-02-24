fe27d364616c77493413a7ed6bf29b38b7430e29
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-02-24 14:42:37 +0300

    Fix api server host

--- web2/app/adapters/application.js
+++ web2/app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'http://localhost:2600',
+  host: location.protocol + '//' + location.hostname + ':' + 2600,
   namespace: 'api/v1',
 });
