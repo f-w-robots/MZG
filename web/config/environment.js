8a5245ff81910e78ffc9884fb5fd38d4686e2c1d
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-02-24 16:47:23 +0300

    Rename web2 to web

--- web/config/environment.js
+++ web/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'web2',
+    modulePrefix: 'web',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
