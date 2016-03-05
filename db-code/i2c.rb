409b53199e91fe104daf890144a74eca3b0e0644
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-03-05 21:38:11 +0300

    Fix bug in i2c car and make stop action on crossroad

--- db-code/i2c.rb
+++ db-code/i2c.rb
@@ -1,23 +1,23 @@
 @answer = []
 # 'r', 'l', 'rr', 'll', 'c', 'f'
 def sensorRaw s
-  if s == 'rr'
-    sensorsValues[4]
+  (if s == 'rr'
+    @sensorsValues[4]
   elsif s == 'll'
-    sensorsValues[0]
+    @sensorsValues[0]
   elsif s == 'r'
-    sensorsValues[3]
+    @sensorsValues[3]
   elsif s == 'l'
-    sensorsValues[1]
+    @sensorsValues[1]
   elsif s == 'c'
-    sensorsValues[2]
+    @sensorsValues[2]
   elsif s == 'f'
-    sensorsValues[5]
-  end
+    @sensorsValues[5]
+  end).to_i
 end
 
 def sensor s
-  sensorRaw(s) >= 4
+  sensorRaw(s) <= 4
 end
 
 def stop
@@ -39,24 +39,40 @@ end
 puts "Start script with #{@hwid}"
 
 socket.send('04INIT')
-socket.send('18!1"0#0$1')
+socket.send('18!0"0#0$0')
+
+@steps = ['f', 'r']
+
+def current_step
+  @steaps[@steapNumebr]
+end
+
+@steapNumebr = 0
 
 loop do
   while msg_empty?
     sleep 0.000001
   end
 
-  sensorsValues = shift_msg
+  a = shift_msg
+  puts "  #{a[5]}  "
+  puts a[0..4]
+  puts '-'*10
+  @sensorsValues = a
 
-  # if sensor('rr') && sensor('ll')
-  #   stop
-  # end
-  start
+  if current_step == 'f'
+    if sensor('rr') && sensor('ll')
+      stop
+      @steapNumebr ++
+    else
+      start
+    end
+  end
 
   if @answer.size != 1
     raise("end loop, @answers count #{@answer.size}")
   else
-    puts sensorsValues
+    # puts @sensorsValues
     socket.send(@answer.shift)
   end
 end
