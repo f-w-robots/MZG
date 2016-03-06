ab597a25143d8e59ce94af66d8ff1a248474ae81
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-03-06 22:34:01 +0300

    Add basic line search algorithm and many updates

--- db-code/i2c.rb
+++ db-code/i2c.rb
@@ -1,3 +1,16 @@
+def wait_message
+  while msg_empty?
+    sleep 0.000001
+  end
+end
+
+def get_and_print_sensors
+  @sensorsValues = shift_msg
+  puts "  #{@sensorsValues[5]}  "
+  puts @sensorsValues[0..4]
+  puts '-'*10
+end
+
 # 'r', 'l', 'rr', 'll', 'c', 'f'
 def sensorRaw s
   (if s == 'rr'
@@ -16,7 +29,7 @@ def sensorRaw s
 end
 
 def sensor s
-  sensorRaw(s) <= 4
+  sensorRaw(s) <= 2
 end
 
 def stop
@@ -47,29 +60,78 @@ def move_forward
   if sensor('rr') && sensor('ll')
     stop
     next_step
-  else
-    start
+    return
   end
+  if sensor('r')
+    right
+    return
+  end
+  if sensor('l')
+    left
+    return
+  end
+  start
+end
+
+def move_left
+  move_turn 'left'
 end
 
 def move_right
+  move_turn 'right'
+end
+
+def move_turn direction
   if @r1
-    if sensor('rr') && sensor('ll')
+    if sensor('rr')
+      @r1a = true
+    end
+    if sensor('ll')
+      @r1b = true
+    end
+    if @r1a && @r1b
       stop
       next_step
     else
-      right
+      send(direction)
     end
   else
     if !sensor('rr') && !sensor('ll')
       @r1 = true
     end
-    right
+    send(direction)
   end
 end
 
 def move_stop
   stop
+end
+
+def move_search
+  if @search_mode_1a
+    if sensor('f') && !sensor('l') && !sensor('r')
+      next_step
+    end
+    left
+    return
+  end
+  if @search_mode_1
+    if sensor('c')
+      @skip_timeout = 20
+      start
+      @search_mode_1a = true
+    else
+      start
+    end
+    return
+  end
+  if sensor('f')
+    start
+    puts 'SSSxxx' * 100
+    @search_mode_1 = true
+    return
+  end
+  start
 end
 
 
@@ -78,22 +140,28 @@ socket.send('04INIT')
 socket.send('18!0"0#0$0')
 
 @answer = []
-@steps = ['forward', 'right', 'stop']
+@steps = ['forward', 'left', 'stop']
+# @steps = ['forward', 'stop']
+# @steps = ['search', 'forward']
+# @steps = ['stop']
 @steapNumebr = 0
 
 loop do
-  while msg_empty?
-    sleep 0.000001
+  wait_message
+
+  get_and_print_sensors
+
+  if(@skip_timeout)
+    @skip_timeout -= 1
+    start
+    if(@skip_timeout == 0)
+      @skip_timeout = nil
+      puts 'ENDTOMOut' * 100
+    end
+  else
+    puts current_step
+    send(:"move_#{current_step}")
   end
-
-  a = shift_msg
-  puts "  #{a[5]}  "
-  puts a[0..4]
-  puts '-'*10
-  @sensorsValues = a
-
-  puts current_step
-  send(:"move_#{current_step}")
 
   if @answer.size != 1
     raise("end loop, @answers count #{@answer.size}")
