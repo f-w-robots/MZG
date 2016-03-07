11619d1bb6701abb80d58e8d2ce7981b0c98a7c0
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-03-07 13:48:15 +0300

    User classes in car line follower algorithm

--- db-code/i2c.rb
+++ db-code/i2c.rb
@@ -1,171 +1,231 @@
+# Work with sensors data
+#
+class Sensors
+  OneZeroDeliver = 2
+
+  def update values, print = false
+    @values = values
+    if print
+      puts "  #{@values[5]}  "
+      puts @values[0..4]
+      puts '-'*10
+    end
+  end
+
+  # 'r', 'l', 'rr', 'll', 'c', 'f'
+  def sensorRaw s
+    (if s == 'rr'
+      @values[4]
+    elsif s == 'll'
+      @values[0]
+    elsif s == 'r'
+      @values[3]
+    elsif s == 'l'
+      @values[1]
+    elsif s == 'c'
+      @values[2]
+    elsif s == 'f'
+      @values[5]
+    end).to_i
+  end
+
+  def value s
+    sensorRaw(s) <= OneZeroDeliver
+  end
+end
+
+# Create answer - command for car
+#
+class Answer
+  def initialize
+    @answer = []
+  end
+
+  def stop
+    @answer.push '18!0"0#0$0'
+  end
+
+  def start
+    @answer.push '18!1"0#0$1'
+  end
+
+  def right
+    @answer.push '18!1"0#1$0'
+  end
+
+  def left
+    @answer.push '18!0"1#0$1'
+  end
+
+  def get
+    if @answer.size != 1
+      raise("end loop, @answers count #{@answer.size}")
+    else
+      @answer.shift
+    end
+  end
+end
+
 def wait_message
   while msg_empty?
     sleep 0.000001
   end
 end
 
-def get_and_print_sensors
-  @sensorsValues = shift_msg
-  puts "  #{@sensorsValues[5]}  "
-  puts @sensorsValues[0..4]
-  puts '-'*10
-end
+@next_step_after_skip = false
 
-# 'r', 'l', 'rr', 'll', 'c', 'f'
-def sensorRaw s
-  (if s == 'rr'
-    @sensorsValues[4]
-  elsif s == 'll'
-    @sensorsValues[0]
-  elsif s == 'r'
-    @sensorsValues[3]
-  elsif s == 'l'
-    @sensorsValues[1]
-  elsif s == 'c'
-    @sensorsValues[2]
-  elsif s == 'f'
-    @sensorsValues[5]
-  end).to_i
-end
-
-def sensor s
-  sensorRaw(s) <= 2
-end
-
-def stop
-  @answer.push '18!0"0#0$0'
-end
-
-def start
-  @answer.push '18!1"0#0$1'
-end
-
-def right
-  @answer.push '18!1"0#1$0'
-end
-
-def left
-  @answer.push '18!0"1#0$1'
-end
-
-def current_step
-  @steps[@steapNumebr]
-end
-
-def next_step
-  @steapNumebr += 1
-end
-
-def move_forward
-  if sensor('rr') && sensor('ll')
-    stop
-    next_step
-    return
+class Mover
+  def initialize sensor, answer, commands
+    @sensor = sensor
+    @answer = answer
+    @commands = commands
   end
-  if sensor('r')
-    right
-    return
+
+  def sensor value
+    @sensor.value value
   end
-  if sensor('l')
-    left
-    return
-  end
-  start
-end
 
-def move_left
-  move_turn 'left'
-end
-
-def move_right
-  move_turn 'right'
-end
-
-def move_turn direction
-  if @r1
-    if sensor('rr')
-      @r1a = true
+  def move_forward
+    if @forward_mode1
+      if (!sensor.value('rr') && !sensor('ll'))
+        @commands.next
+      end
+      @answer.start
     end
-    if sensor('ll')
-      @r1b = true
+    if (sensor('rr') && sensor('ll')) || (sensor('r') && sensor('l'))
+      @forward_mode1 = true
+      @answer.start
+      @commands.next
+      return
     end
-    if @r1a && @r1b
-      stop
-      next_step
+    if sensor('r')
+      @answer.start
+      return
+    end
+    if sensor('l')
+      @answer.start
+      return
+    end
+    @answer.start
+  end
+
+  def move_left
+    move_turn 'left'
+  end
+
+  def move_right
+    move_turn 'right'
+  end
+
+  def move_stop
+    @answer.stop
+  end
+
+  def move_search
+    if @search_mode_1a
+      if sensor('f') && !sensor('l') && !sensor('r')
+        @commands.next
+      end
+      @answer.left
+      return
+    end
+    if @search_mode_1
+      if sensor('c')
+        skip_timeout 20, ->{start}
+        @answer.start
+        @search_mode_1a = true
+      else
+        @answer.start
+      end
+      return
+    end
+    if sensor('f')
+      @answer.start
+      puts 'SSSxxx' * 100
+      @search_mode_1 = true
+      return
+    end
+    @answer.start
+  end
+
+  private
+  def move_turn direction
+    if @r1
+      if sensor('rr')
+        @r1a = true
+      end
+      if sensor('ll')
+        @r1b = true
+      end
+      if @r1a && @r1b
+        skip_timeout 120, ->{send(direction)}
+        @answer.left
+        @commands.next
+      else
+        @answer.send(direction)
+      end
     else
-      send(direction)
+      if !sensor('rr') && !sensor('ll')
+        @r1 = true
+      end
+      @answer.send(direction)
     end
-  else
-    if !sensor('rr') && !sensor('ll')
-      @r1 = true
-    end
-    send(direction)
   end
 end
 
-def move_stop
-  stop
+class Commands
+  def initialize
+    @steps = ['forward', 'right', 'stop']
+    @stepNumebr = 0
+  end
+
+  def next
+    @stepNumebr += 1
+  end
+
+  def current
+    @steps[@stepNumebr]
+  end
 end
 
-def move_search
-  if @search_mode_1a
-    if sensor('f') && !sensor('l') && !sensor('r')
-      next_step
-    end
-    left
-    return
-  end
-  if @search_mode_1
-    if sensor('c')
-      @skip_timeout = 20
-      start
-      @search_mode_1a = true
-    else
-      start
-    end
-    return
-  end
-  if sensor('f')
-    start
-    puts 'SSSxxx' * 100
-    @search_mode_1 = true
-    return
-  end
-  start
+def skip_timeout delay, action
+  @skip_timeout = delay
+  @skip_timeout_action = action
 end
 
+def stop!
+  @steps = ['stop']
+  @steapNumebr = 0
+  @exit = true
+  puts 'STOP___'*10
+end
+
+@answer = Answer.new
+@sensors = Sensors.new
+@commands = Commands.new
+@mover = Mover.new @sensors, @answer, @commands
 
 puts "Start script with #{@hwid}"
 socket.send('04INIT')
 socket.send('18!0"0#0$0')
 
-@answer = []
-@steps = ['forward', 'left', 'stop']
-# @steps = ['forward', 'stop']
-# @steps = ['search', 'forward']
-# @steps = ['stop']
-@steapNumebr = 0
-
 loop do
   wait_message
 
-  get_and_print_sensors
+  @sensors.update shift_msg, true
 
   if(@skip_timeout)
     @skip_timeout -= 1
-    start
-    if(@skip_timeout == 0)
-      @skip_timeout = nil
-      puts 'ENDTOMOut' * 100
-    end
+    @skip_timeout_action.call
+    @skip_timeout = nil if @skip_timeout == 0
   else
-    puts current_step
-    send(:"move_#{current_step}")
+    puts @commands.current
+    @mover.send(:"move_#{@commands.current}")
   end
 
-  if @answer.size != 1
-    raise("end loop, @answers count #{@answer.size}")
-  else
-    socket.send(@answer.shift)
+  socket.send(@answer.get)
+
+  if @exit
+    break
   end
 end
