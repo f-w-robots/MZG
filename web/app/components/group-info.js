24cb1981b383d23aaa9ac40c03a3f0551c93afea
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-03-08 20:49:30 +0300

    Show group info

---
+++ web/app/components/group-info.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  info: null,
+  url: null,
+
+  currentHost: function(port) {
+    return location.protocol + '//' + location.hostname + ':' + port;
+  },
+
+  didInsertElement: function() {
+    var self = this;
+
+    setInterval(function() {
+      Ember.$.get(self.currentHost('2500') + '/group/info/game', function(data) {
+        self.set('info', data);
+      }).fail(function() {
+        self.set('info', 'group not runned');
+      });
+    }, 1000);
+  },
+});
