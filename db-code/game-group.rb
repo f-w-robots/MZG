64fecbde94e399ab201fa46bfc56d8a025e44eeb
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-03-09 13:05:09 +0300

    Update game

--- db-code/game-group.rb
+++ db-code/game-group.rb
@@ -9,6 +9,9 @@
 
     @messages = {}
     @crashed = {}
+    @finished = {}
+
+    @responses = {}
   end
 
   def start
@@ -20,37 +23,7 @@
           sleep 0.001
           @options[:info][:timout] = theend - Time.now
           if theend < Time.now
-            allow_accept(false)
-            @options[:commands].keys.each do |key|
-              if @crashed[key]
-                @crashed[key] = false
-                out_msg_left 'restore', key
-              end
-              @options[:commands][key].each do |command|
-                out_msg_left command, key
-                @messages[key] ||= 0
-                @messages[key] += 1
-              end
-              @options[:commands][key] = []
-            end
-
-            # Wait responses
-            while true
-              puts @messages
-              count = 0
-              @messages.each do |k, v|
-                count += v
-              end
-              break if count <= 0
-              sleep 0.1
-            end
-
-            allow_accept
-
-            if round >= @rounds
-              finish
-              destroy
-            end
+            start_round(round)
 
             break
           end
@@ -59,10 +32,87 @@
     end
   end
 
+  def start_round round
+    allow_accept(false)
+    @options[:commands].keys.each do |hwid|
+      commands = @options[:commands][hwid]
+      @options[:commands][hwid] = []
+      if @crashed[hwid]
+        commands.unshift('restore')
+        @crashed[hwid] = false
+      end
+      @finished[hwid] = false
+      @responses[hwid] = []
+      @responses[hwid].push 'first'
+      send_commands commands, hwid
+    end
+
+    # Wait responses
+    loop do
+      all_finish = true
+      puts @finished
+
+      @finished.keys.each do |hwid|
+        if !@finished[hwid]
+          all_finish = false
+        end
+      end
+      puts all_finish
+      if all_finish
+        break
+      end
+
+      sleep 0.5
+    end
+
+    allow_accept
+
+    if round >= @rounds
+      finish
+      destroy
+    end
+  end
+
+  def send_commands commands, hwid
+    thread = Thread.new do
+      commands.each do |command|
+        loop do
+          sleep 0.1
+          if @responses[hwid].size > 0
+            if @responses[hwid].shift == 'crash'
+              @finished[hwid] = true
+              thread.terminate
+            end
+            break
+          end
+        end
+        out_msg_left command, hwid
+        @messages[hwid] ||= 0
+        @messages[hwid] += 1
+      end
+
+      loop do
+        sleep 0.1
+        if @responses[hwid].size > 0
+          break
+        end
+
+        @finished[hwid] = true
+      end
+    end
+  end
+
   def in_msg_left msg, hwid
     @options[:info][:score] ||= {}
     @options[:info][:score][hwid] ||= 0
     @options[:info][:score][hwid] += 1
+
+    @options[:info][:waits] ||= {}
+    @options[:info][:waits][hwid] ||= 0
+    @options[:info][:waits][hwid] += 1
+
+    @responses[hwid].push msg
+
     clear_stack hwid, msg
     out_msg_right(msg, hwid)
   end
