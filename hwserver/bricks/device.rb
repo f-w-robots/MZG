09b66b4b4bad4a9543d11fdf1842b798cdaf7c68
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-04-09 11:42:04 +0300

    hotfix for retrive aborted device

--- hwserver/bricks/device.rb
+++ hwserver/bricks/device.rb
@@ -30,20 +30,22 @@ class Device < Brick
   end
 
   def start_abort_control abort_timeout
-    Thread.new do
-      loop do
-        sleep 0.001
-        if @send_to_device_time && @send_to_device_time.to_f < (Time.now.to_f - abort_timeout)
-          puts "ABORT!, retrive"
-          send_to_device(@latest_message)
-        end
-      end
-    end
+    # Thread.new do
+    #   loop do
+    #     sleep 0.001
+    #     if @send_to_device_time && @send_to_device_time.to_f < (Time.now.to_f - abort_timeout)
+    #       puts "ABORT!, retrive"
+    #       @send_to_device_time = Time.now
+    #       send_to_device(@latest_message)
+    #     end
+    #   end
+    # end
   end
 
   def in_msg_right msg, hwid
     if msg.start_with?('MAX_TIMEOUT:')
-      start_abort_control(msg.sub('MAX_TIMEOUT:').to_f)
+      start_abort_control(msg.sub('MAX_TIMEOUT:', '').to_f)
+      return
     end
     send_to_device msg
   end
