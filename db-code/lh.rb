64fa4b1658476cb60ec5d7183ecb895c8e196bae
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-05-02 15:31:52 +0300

    New code for lighthouse

--- db-code/lh.rb
+++ db-code/lh.rb
@@ -5,25 +5,57 @@ class Worker
     @connection = connection
 
     @cards = {}
+
+    @registred_hwids = []
+
+    @dict = {
+      ["93", "87", "8c", "65"] => "bug3",
+      ["65", "7C", "8C", "65"] => "bug2",
+    }
+
+    puts 'inited'
+    @connection.to_device("0")
   end
 
   def from_device msg
     if(msg.start_with?('id:'))
-      id = msg.sub('id:','')
-      @cards.each do |card, device|
-        if !device
-          @cards[card] = msg
-          @connection.to_device("0" + id)
-          puts "#{card.bytes.map{|a|a.to_s(16)}.inspect}}: #{id}"
-          break
+      hwid = msg.sub('id:','')
+      puts hwid
+      if !@registred_hwids.include?(hwid)
+        @dict.each do |card, device|
+          if device == hwid && @cards.has_key?(card)
+            @cards[card] = hwid
+            @registred_hwids.push hwid
+            @connection.to_device("0" + @registred_hwids.join(10.chr))
+            puts @registred_hwids.join(13.chr).inspect
+            puts "Assign #{hwid} to #{card}"
+          end
         end
+
+        if !@registred_hwids.include?(hwid)
+          puts "No free cards for #{hwid}"
+        end
+      else
+        puts "skip #{hwid}"
       end
     else
-      if(!@cards.has_key?(msg))
-        puts "New card: #{msg.bytes.map{|a|a.to_s(16)}.inspect}"
+      card_id = uuid(msg)
+      # puts "#{rand(1000)}card: #{card_id}"
+      if(!@cards.has_key?(card_id) && card_id != ["c", "39", "ff", "3f"])
+        puts "New card: #{card_id}"
+        @cards[card_id] ||= nil
       end
-      @cards[msg] ||= nil
+
+      if @cards.has_key?(card_id) && @cards[card_id]
+        puts "MAIL to #{@cards[card_id]}"
+        @connection.mail_to(@cards[card_id], 'card')
+      end
     end
+  end
+
+  private
+  def uuid msg
+    msg.bytes.map{|a|a.to_s(16)}
   end
 end
 
