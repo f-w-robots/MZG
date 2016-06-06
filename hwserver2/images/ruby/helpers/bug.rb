883c3b451ad451831475259e1e141f522647af16
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-06-06 16:29:00 +0300

    Binary And Text Package generator

--- hwserver2/images/ruby/helpers/bug.rb
+++ hwserver2/images/ruby/helpers/bug.rb
@@ -1,5 +1,5 @@
 module BUG
-  class Package
+  class PackageBin
     def self.g controller_id, data
       ([controller_id, data.size] + data).pack('c*')
     end
@@ -28,6 +28,34 @@ module BUG
     #   end
     #   g(1, "&#{count}")
     # end
+  end
+
+  class Package
+    def self.g controller_id, data
+      "#{controller_id}#{data.length}#{data}"
+    end
+
+    def self.right_left_wheel right, left
+      g(1, "#{right == 1 ? '!0"1' : (right == -1 ? '!1"0' : '!0"0')}" +
+        "#{left == 1 ? '#1$0' : (left == -1 ? '#0$1' : '#0$0')}")
+    end
+
+    def self.calibrate threshold
+      if threshold == :down || threshold == :black
+        self.g(1, "%1")
+      elsif threshold == :up || threshold == :white
+        self.g(1, "%2")
+      else
+        raise "Unknow threshold: #{threshold}"
+      end
+    end
+
+    def self.setSensorsCount count
+      if count > 9 || count < 1
+        raise "Count"
+      end
+      g(1, "&#{count}")
+    end
   end
 
   class Sensor
